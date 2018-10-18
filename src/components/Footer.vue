26bcff7762ba9147a39bb9fc118088b29b43c29a
no1harm <chen09869@gmail.com>
2018-10-18 18:54:38 +0800

    创建 header/footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer">
+      <p>Footer</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'footer',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  align-self: end;
+  background-color: #d7d7d7;
+  color: #666;
+  font-size: 13px;
+  padding: 10px;
+  text-align: center;
+}
+</style>
