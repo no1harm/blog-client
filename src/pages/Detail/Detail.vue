a9872579a2ec2e5110776b7f38de3adddc602c8d
no1harm <chen09869@gmail.com>
2018-10-18 20:33:30 +0800

    完成 Detail 的结构和样式

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-      Detail
+  <div id="detail">
+    <section class="user-info">
+      <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="" class="avatar">
+      <h3>前端异步大揭秘</h3>
+      <p><router-link to="/user">若愚</router-link> 发布于3天前</p>
+    </section>
+    <section class="article">
+    </section>
   </div>
 </template>
 
@@ -18,5 +24,54 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang='less'>
+@import "../../assets/base.less";
+// @import "../../assets/article.less";
+
+#detail {
+  .user-info {
+    display: grid;
+    grid: auto auto / 80px 1fr;
+
+    margin-top: 30px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #ebebeb;
+
+    .avatar {
+      grid-column: 1;
+      grid-row: 1 / span 2;
+
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+    }
+
+    h3 {
+      grid-column: 2;
+      grid-row: 1;
+      
+      margin: 5px 0;
+
+    }
+
+    p {
+      grid-column: 2;
+      grid-row: 2;
+
+      margin-top: 0;
+      font-size: 12px;
+      color: @textLighterColor;
+
+      a {
+        color: @themeColor;
+        text-decoration: none;
+      }
+    }
+  }
+
+  .article {
+    padding: 30px 0;
+  }
+
+}
 </style>
