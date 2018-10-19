b8624c049ed3d3ea8b9e5331dc98b17b8bcdb8f3
no1harm <chen09869@gmail.com>
2018-10-19 12:53:19 +0800

    使用 vuex

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,24 +4,56 @@
       <h1>Let's Share</h1>
       <p>精品博客汇聚</p>
       <div class="btns">
-        <el-button>立即注册</el-button>
-        <el-button>立即登录</el-button>
+        <router-link to="/register"><el-button>立即注册</el-button></router-link> 
+        <router-link to="/login"><el-button>立即登录</el-button></router-link>
       </div>
     </template>
     <template v-else >
       <h1>Let's Share</h1>
       <i class="edit el-icon-edit"></i>   
+      <div class="user">
+        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username"> 
+        <ul>
+            <li><router-link to="my">我的</router-link></li>
+            <li><a href="#" @click="onLogout">注销</a></li>
+          </ul>        
+      </div> 
     </template>
   </header>
 </template>
 
 <script>
+// 映射
+import {mapGetters,mapActions} from 'vuex'
+
 export default {
   name:'Header',
   data() {
     return {
-      isLogin:false
     }
+  },
+
+  // 把 store 中 getter 属性映射到此组件
+  computed:{
+    ...mapGetters([
+      'isLogin',
+      'user'
+    ])
+  },
+
+  //在页面没有渲染之前检查用户是否登录
+  created(){
+    this.checkLogin()
+  },
+  methods:{
+    // 把 auth.js 中的 checkLogin 方法映射到此组件
+    ...mapActions([
+      'checkLogin',
+      'logout'
+    ]),
+    onLogout() {
+        this.logout()
+      }
   },
   components: {
 
@@ -83,6 +115,37 @@ header.login {
     border: 1px solid #fff;
     border-radius: 50%;
     margin-left: 15px;
+  }
+  .user {
+    position: relative;
+
+    ul {
+      display: none;
+      position: absolute;
+      right: 0;
+      list-style: none;
+      border: 1px solid #eaeaea;
+      margin:0;
+      padding: 0;
+      background-color: #fff;
+
+      a {
+        text-decoration: none;
+        color: #333;
+        font-size: 12px;
+        display: block;
+        padding: 5px 10px;
+
+        &:hover {
+          background-color: #eaeaea;
+        }
+      }
+
+    }
+
+    &:hover ul {
+      display: block;
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
+import store from './store'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
@@ -12,6 +13,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
